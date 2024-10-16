1599ae279e850ea2ad387d3e3d4c42bd93fb08c6
rsekoua <42903463+rsekoua@users.noreply.github.com>
2024-10-16 23:01:14 +0000

    Update ListMateriels.php

--- app/Filament/Resources/MaterielResource/Pages/ListMateriels.php
+++ app/Filament/Resources/MaterielResource/Pages/ListMateriels.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\MaterielResource\Pages;
 
 use App\Filament\Resources\MaterielResource;
+use App\Models\Materiel;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+//use Filament\Resources\Pages\ListRecords\Tab;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListMateriels extends ListRecords
 {
@@ -14,6 +18,25 @@ class ListMateriels extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('Tout le materiel'),
+            'Ordinateurs' => Tab::make()
+                            //->icon('heroicon-m-user-group')
+                            ->badge(Materiel::query()->where('type', '=', 'ordinateur')->count())
+                            ->modifyQueryUsing(fn ($query) =>$query->where('type', '=', 'ordinateur')),
+            'Imprimantes' => Tab::make()
+                //->icon('heroicon-m-user-group')
+                ->badge(Materiel::query()->where('type', '=', 'imprimante')->count())
+                ->modifyQueryUsing(fn ($query) =>$query->where('type', '=', 'imprimante')),
+            'Vidéos Projecteurs' => Tab::make()
+                //->icon('heroicon-m-user-group')
+                ->badge(Materiel::query()->where('type', '=', 'videoprojecteur')->count())
+                ->modifyQueryUsing(fn ($query) =>$query->where('type', '=', 'videoprojecteur')),
         ];
     }
 }
